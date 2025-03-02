be7d16ebfe7fc95e0a3ba06c966ce649232737ff
Nicolae Postica <nicolae.postica2@gmail.com>
2025-03-02 19:12:58 +0200

    validate registered data (ref ODU-82)

--- asterisk_plus/models/settings.py
+++ asterisk_plus/models/settings.py
@@ -23,6 +23,10 @@ RECORDING_ACCESS_SELECTION = [
     ('s3', 'S3 Storage link'),
 ]
 
+required_fields = [
+    'admin_email', 'admin_name', 'admin_phone', 'company_name', 'company_city', 'company_email', 'company_phone',
+    'company_country_code','company_country', 'company_state_name', 'company_country_name', 'installation_date',
+    'module_name', 'module_version', 'odoo_url', 'odoo_version']
 
 PREPAID_PAYMENT_URL = 'https://buy.stripe.com/aEU01VaER5D15lC4gj'
 # Starting from Odoo 12.0 there is admin user with ID 2.
@@ -258,13 +262,6 @@ class Settings(models.Model):
         admin_email = self.get_param('admin_email')
         admin_phone = self.get_param('admin_phone')
         company_email = self.get_param('company_email')
-        if not self.get_param('company_country'):
-            raise ValidationError('Please enter your company country!')
-        if not company_email or not admin_email or not admin_phone:
-            raise ValidationError('Please enter all required fields: company email, '
-                                  'your email, and your phone!')
-        if admin_email == 'admin@example.com' or company_email == 'admin@example.com':
-            raise ValidationError('Please set your real email address, not admin@example.com.')
         data = {
             'company_name': self.get_param('company_name'),
             'company_country': self.get_param('company_country'),
@@ -284,6 +281,14 @@ class Settings(models.Model):
             'installation_date': self.get_param('installation_date').strftime("%Y-%m-%d"),
             'partner_code': self.get_param('partner_code'),
         }
+        missing_fields = [field for field in required_fields if field not in data or not data[field]]
+        if missing_fields:
+            raise ValidationError(f"Missing required fields: {', '.join(missing_fields)}")
+        if not company_email or not admin_email or not admin_phone:
+            raise ValidationError('Please enter all required fields: company email, '
+                                  'your email, and your phone!')
+        if admin_email == 'admin@example.com' or company_email == 'admin@example.com':
+            raise ValidationError('Please set your real email address, not admin@example.com.')
         res = self.make_api_request(self.get_param('api_url'), requests.post, data=data)
         if not res and self.get_param('api_fallback_url'):
             # Make a request and give error if fallback API endpoint is not available.
